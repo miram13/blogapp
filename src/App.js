090299bd01f22f000bdaa6683fe7b7ee2818d609
Mohammadi Iram <mohammadiiram@gmail.com>
2021-12-16 09:47:04 +0530

    axios method

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import '@patternfly/react-core/dist/styles/base.css';
+import './App.css';
+import { ListBlog } from './ListBlog';
+import { PostBlog } from './PostBlog';
+
+function App() {
+
+  return (
+    <div className="App">
+      <PostBlog />
+      <ListBlog />
+    </div>
+  );
+}
+
+export default App;
